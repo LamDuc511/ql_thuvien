ca62baaadca471614e03f6cf61df88ee9415b765
LamDuc511 <132547260+LamDuc511@users.noreply.github.com>
2023-05-04 13:51:05 +0700

    Add files via upload

---
+++ library/Forms/JConnection.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library.Forms;
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ADMIN
+ */
+public class JConnection {
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");
+            return con;
+            
+        }catch(Exception e){JOptionPane.showMessageDialog(null, e);
+                               return null;}
+    }
+}
