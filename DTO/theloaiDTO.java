a5fdc47ee42e34340f6a4809d496421a260245ce
LamDuc511 <132547260+LamDuc511@users.noreply.github.com>
2023-05-11 14:01:20 +0700

    Add files via upload

---
+++ DTO/theloaiDTO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class theloaiDTO {
+    int ma;
+    String ten;
+
+    public theloaiDTO(int ma, String ten) {
+        this.ma = ma;
+        this.ten = ten;
+    }
+
+    public theloaiDTO() {
+    }
+
+    public int getMa() {
+        return ma;
+    }
+
+    public void setMa(int ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+    
+}
